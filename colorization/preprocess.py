efa338928fbb4b15fbf503080d35c2bf969f522e
root <root@caffe-python3-cuda9-1-vm.c.group-13-202519.internal>
2018-05-25 12:33:25 +0000

    Fixed preprocess.py

--- colorization/preprocess.py
+++ colorization/preprocess.py
@@ -1,25 +1,31 @@
-import sys, glob
+import sys, os, glob
 
 # Find JPEG images in every folder and append list of images to next index
-def findImages(image_paths):
+def findImages(image_paths, owd):
     file_list = []
     for path in image_paths:
-        file_list.append(glob.glob(path + "*.jpeg"))
+        print(path)
+        os.chdir(path)
+        print(glob.glob("*.JPEG"))
+        file_list.append(glob.glob(path + "*.JPEG"))
+        os.chdir(owd)
     return file_list
 
 def writeFile(output_file, image_list):
     with open(output_file, 'w') as file:
         for index, images in enumerate(image_list):
             for image in images:
-                file.write(image + " " + str(index) + "\n")
+                file.write(os.path.basename(image) + " " + str(index) + "\n")
 
 def main():
     if (len(sys.argv) < 3):
         return
 
+    owd = os.getcwd()
     output_file = str(sys.argv[1])
-    image_paths = str(sys.argv[2:])
-    image_list = findImages(image_paths)
+    print(sys.argv[2:])
+    image_paths = sys.argv[2:]
+    image_list = findImages(image_paths, owd)
     writeFile(output_file, image_list)
 
 # py preprocess.py outputfile /path/to/images/
